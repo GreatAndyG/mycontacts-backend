570e9851f8b215abe69d5865918277fecfbcf63d
GreatAndyG <Andrewgibs@outlook.com>
2024-04-07 13:07:47 +1000

    Add error Handling Middleware

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const errorHandler = require("./middleware/errorhandler");
 
 const dotenv = require("dotenv").config();
 
@@ -9,6 +10,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 
 app.use("/api/contacts", require("./routes/contactRoutes"));
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Sever running on port ${port}`);
