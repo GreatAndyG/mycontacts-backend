a44ebd1cd59f1f2f6e473443e8deb6c2ddb4fff8
GreatAndyG <Andrewgibs@outlook.com>
2024-04-09 21:09:58 +1000

    Add sever.js comments, add contact model sechema

--- server.js
+++ server.js
@@ -1,17 +1,24 @@
+// Import required modules
+
 const express = require("express");
 const errorHandler = require("./middleware/errorhandler");
 const connectDb = require("./config/dbConnections");
 const dotenv = require("dotenv").config();
+
+// Establish connection to database
 connectDb();
+// Create an instance of express
 const app = express();
 
+// Define a port to listen on
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
 
+// Define a route
 app.use("/api/contacts", require("./routes/contactRoutes"));
 app.use(errorHandler);
-
+// Start the server
 app.listen(port, () => {
   console.log(`Sever running on port ${port}`);
 });
