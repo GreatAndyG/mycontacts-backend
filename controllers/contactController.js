d1207b7afff30e8a1781d44df8447e9cf04e7a27
GreatAndyG <Andrewgibs@outlook.com>
2024-04-15 18:55:16 +1000

    Add usercontroller(login user). Add validate token security. Completed crud of contacts.

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -3,15 +3,15 @@ const Contact = require("../models/contactModel");
 const validation = require("../utilities/validation");
 //@desc Get all contacts
 //@route GET /api/contacts
-//@access public
+//@access private
 const getContacts = asyncHandler(async (req, res) => {
-  const contacts = await Contact.find({});
+  const contacts = await Contact.find({ user_id: req.user.id });
   res.status(200).json(contacts);
 });
 
 //@desc Create New contact
 //@route POST /api/contacts
-//@access public
+//@access private
 const createContact = asyncHandler(async (req, res) => {
   console.log("The request body is:", req.body);
   const { name, email, phone } = req.body;
@@ -22,6 +22,7 @@ const createContact = asyncHandler(async (req, res) => {
     name,
     email,
     phone,
+    user_id: req.user.id,
   });
 
   try {
@@ -34,7 +35,7 @@ const createContact = asyncHandler(async (req, res) => {
 
 //@desc Get contact
 //@route GET /api/contacts/:id
-//@access public
+//@access private
 const getContact = asyncHandler(async (req, res) => {
   const id = req.params.id;
   if (!validation.isIdValidMongo(id)) {
@@ -50,21 +51,39 @@ const getContact = asyncHandler(async (req, res) => {
 
 //@desc Update contact
 //@route PUT /api/contacts/:id
-//@access public
+//@access private
 const updateContact = asyncHandler(async (req, res) => {
   const id = req.params.id;
   if (!validation.isIdValidMongo(id)) {
     return res.status(400).json({ error: "Invalid contact ID format" });
   }
 
-  const updates = req.body;
+  try {
+    // Retrieve the contact from the database
+    const contact = await Contact.findById(id);
 
-  const contact = await Contact.findByIdAndUpdate(id, updates, { new: true });
+    // Check if the contact exists
+    if (!contact) {
+      return res.status(404).json({ error: "Contact not found" });
+    }
 
-  if (!contact) {
-    return res.status(404).json({ error: `Contact not found` });
+    // Check if the user has permission to update the contact
+    if (contact.user_id.toString() !== req.user.id) {
+      return res.status(403).json({
+        error: "User doesn't have permission to update other user contacts",
+      });
+    }
+
+    // Proceed with updating the contact if user has permission
+    const updatedContact = await Contact.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+
+    res.status(200).json(updatedContact);
+  } catch (error) {
+    console.error("Error updating contact:", error);
+    res.status(500).json({ error: "Failed to update contact" });
   }
-  res.status(200).json(contact);
 });
 //@desc Delete contact
 //@route DELETE /api/contacts/:id
@@ -74,12 +93,30 @@ const deleteContact = asyncHandler(async (req, res) => {
   if (!validation.isIdValidMongo(id)) {
     return res.status(400).json({ error: "Invalid contact ID format" });
   }
-  const contact = await Contact.findByIdAndDelete(id);
 
-  if (!contact) {
-    return res.status(404).json({ error: `Contact not found` });
+  try {
+    // Find the contact by ID and delete it
+    const contact = await Contact.findByIdAndDelete(id);
+
+    // Check if contact exists
+    if (!contact) {
+      return res.status(404).json({ error: "Contact not found" });
+    }
+
+    // Check if the user has permission to delete the contact
+    if (contact.user_id.toString() !== req.user.id) {
+      return res.status(403).json({
+        error: "User doesn't have permission to delete other user contacts",
+      });
+    }
+
+    // If all checks pass, send success response
+    res.status(200).json({ message: "Contact deleted successfully" });
+  } catch (error) {
+    console.error("Error deleting contact:", error);
+    // Send error response
+    res.status(500).json({ error: "Failed to delete contact" });
   }
-  res.status(200).json({ message: "Contact deleted succesfully" });
 });
 
 module.exports = {
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
 
 //@desc Register a user
@@ -43,6 +44,28 @@ const registerUser = asyncHandler(async (req, res) => {
 //@route Post /api/users/login
 //@access public
 const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json({ error: "All fields are mandatory!" });
+  }
+  const user = await User.findOne({ email });
+  //Compare password with hashed password
+  if (user && (await bcrypt.compare(password, user.password))) {
+    const accessToken = jwt.sign(
+      {
+        user: {
+          username: user.username,
+          email: user.email,
+          id: user.id,
+        },
+      },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: "15m" }
+    );
+    res.status(200).json({ accessToken });
+  } else {
+    res.status(401).json({ error: "Email or password is not valid" });
+  }
   res.json({ message: "Login user" });
 });
 
@@ -50,7 +73,7 @@ const loginUser = asyncHandler(async (req, res) => {
 //@route Post /api/users/current
 //@access private
 const currentUser = asyncHandler(async (req, res) => {
-  res.json({ message: "Current user information" });
+  res.json(req.user);
 });
 
 module.exports = { registerUser, loginUser, currentUser };
